9359889440b056910f199d334a088c8dd744f001
PhantomOffKanagawa <harry@surmafamily.com>
2025-02-25 19:00:03 -0600

    Initial Commit

---
+++ image_recognition/happy.py
@@ -0,0 +1,5 @@
+
+
+
+def main():
+    print("HELLO ROS2 FROM process_imu package!!!")
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+package_name = 'image_recognition'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    #packages=[package_name]
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='root',
+    maintainer_email='root@todo.todo',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+#		'line_detector = image_recognition.image-detection-script:main',
+		'line_detector = image_recognition.happy:main',
+        ],
+    },
+)
